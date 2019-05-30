e2ca627d90cea1410d24f198490a7b4b52ad481b
Roman Bulinski <aboandrus@gmail.com>
2019-05-30 08:12:28 +0200

    Implemented main method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,26 @@
 public class Main {
 
+    public static int[][] cartesianNeighbor(int x, int y) {
+
+        int[][] result = new int[9][];
+
+        int counter = 0;
+        int counter2 = 0;
+        x = x - 1;
+        y = y - 1;
+
+        while (counter < 9) {
+            while (counter2 < 3) {
+            result[counter][0] = x;
+            result[counter][1] = y;
+            x++;
+                counter2++;
+            }
+            y++;
+            counter2++;
+        }
+        return result;
+    }
+
+
 }
